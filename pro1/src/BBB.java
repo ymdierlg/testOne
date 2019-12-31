36c94a15a6e67c04efc3e7fbae7d97dd6e9f5564
ClassOne-t1 <jintian0xia6@163.com>
2019-12-31 11:38:40 +0800

    asdsadasda

--- pro1/src/BBB.java
+++ pro1/src/BBB.java
@@ -1,6 +1,18 @@
 public class BBB {
     public static void main(String[] args) {
         System.out.println("ssdfdsfdsfdsf");
+        System.out.println("ssdfdsfdsfdsf");
+        System.out.println("ssdfdsfdsfdsf");
+        System.out.println("ssdfdsfdsfdsf");
+        System.out.println("ssdfdsfdsfdsf");
+        System.out.println("ssdfdsfdsfdsf");
+        System.out.println("ssdfdsfdsfdsf");
+        System.out.println("ssdfdsfdsfdsf");
+        System.out.println("ssdfdsfdsfdsf");
+        System.out.println("ssdfdsfdsfdsf");
+        System.out.println("ssdfdsfdsfdsf");
+        System.out.println("ssdfdsfdsfdsf");
+
     }
 
 }
